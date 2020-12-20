a6bf10c7a917fc9ec0b7cd25437163354cec4cc2
Ian Robinson <ianrobinson101@gmail.com>
2020-12-20 17:56:24 +0000

    creating initial build:
    • Home and about page created
    • Layout components made for pages
    • Typography component added to utils
    • Added typography plugin into site config

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+    <Layout>
+        <div>
+            <h1>About Pandas Eating Lots</h1>
+            <p>
+                We're the only site running on your computer dedicated to showing the
+                best photos and videos of pandas eating lots of food.
+            </p>
+        </div>
+    </Layout>
+}
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import kirkhamTheme from "typography-theme-kirkham"
+
+const typogrpahy = new Typography(kirkhamTheme)
+
+export default typogrpahy
+export const rhythm = typorgraphy.rhythm
