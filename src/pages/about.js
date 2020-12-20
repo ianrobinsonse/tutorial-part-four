1fe3a3f5f4d039bbdd57b5b54c37a5f69afae1e1
Ian Robinson <ianrobinson101@gmail.com>
2020-12-20 18:11:02 +0000

    missed out the function call to return.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default function About() {
+  return (
     <Layout>
-        <div>
-            <h1>About Pandas Eating Lots</h1>
-            <p>
-                We're the only site running on your computer dedicated to showing the
-                best photos and videos of pandas eating lots of food.
-            </p>
-        </div>
+      <div>
+        <h1>About Pandas Eating Lots</h1>
+        <p>
+          We're the only site running on your computer dedicated to showing the
+          best photos and videos of pandas eating lots of food.
+        </p>
+      </div>
     </Layout>
+  )
 }
